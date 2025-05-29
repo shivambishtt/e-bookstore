7825b82869dbd0208daee023e12b832a0c2c6055
Shivam Bisht <rajshivam2745@gmail.com>
2025-05-29 23:59:40 +0530

    db file add

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,1 +1,15 @@
 import mongoose from "mongoose";
+
+const MONGODB_URI = process.env.MONGODB_URI
+if (!MONGODB_URI) {
+    throw new Error("Check your database connection string")
+}
+let cached = global.mongoose
+
+if (!cached) {
+    cached = global.mongoose = { conn: null, promise: null }
+}
+
+export async function connectDB(){
+    u
+}
