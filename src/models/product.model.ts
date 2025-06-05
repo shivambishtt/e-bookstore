c109333a8a15592db337df348f437aa0dc37377a
Shivam Bisht <rajshivam2745@gmail.com>
2025-06-05 23:48:42 +0530

    image variation schema add

--- src/models/product.model.ts
+++ src/models/product.model.ts
@@ -1,2 +1,30 @@
 import mongoose, { Schema } from "mongoose";
 import bcrypt from "bcrypt"
+
+export interface IProduct {
+    productType: string,
+}
+
+export const imageVariationSchema = new Schema({
+    type: {
+        type: String,
+        required: true,
+        enum: ["SQUARE", "WIDE", "PORTRAIT"]
+    },
+    price: {
+        type: Number,
+        required: false,
+        min: 0
+    },
+    license: {
+        type: String
+    }
+
+})
+export const ProductSchema = new Schema({
+
+})
+
+
+
+const User = mongoose.models?.Product || mongoose.model<IProduct>("Product", ProductSchema)
