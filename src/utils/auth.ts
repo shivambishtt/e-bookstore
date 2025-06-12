82758d636177d8343ddab752fb606a1480d215af
Shivam Bisht <rajshivam2745@gmail.com>
2025-06-12 23:55:02 +0530

    session and jwt add

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -45,5 +45,19 @@ export const authOptions: NextAuthOptions = {
                 }
             }
         })
-    ]
+    ],
+    callbacks: {
+        async jwt({ token, user }) {
+            if (user) {
+                token.id = user.id
+                token.role = user.role
+            }
+            return token
+        },
+        async session({ session, token }) {
+            session.user.id = token.id as string;
+            session.user.role = token.role as string;
+            return session
+        }
+    }
 }
