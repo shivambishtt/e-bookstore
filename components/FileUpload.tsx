833b862839b76db115fd650a4623ed8718645b95
Shivam Bisht <rajshivam2745@gmail.com>
2025-06-19 23:58:29 +0530

    fileupload component changes

--- components/FileUpload.tsx
+++ components/FileUpload.tsx
@@ -3,7 +3,7 @@ import { IKUpload } from "imagekitio-next"
 import { IKUploadResponse } from "imagekitio-next/dist/types/components/IKUpload/props"
 import { useState } from "react"
 
-function FileUpload() {
+function FileUpload({ onSuccess }: { onSuccess: (response: IKUploadResponse) => void }) {
     const [uploading, setUploading] = useState<boolean>(false)
     const [error, setError] = useState<string | null>(null)
 
@@ -15,10 +15,23 @@ function FileUpload() {
     const handleSuccess = (response: IKUploadResponse) => {
         setUploading(false)
         setError(null)
+        onSuccess(response)
+    }
+    const handleStartUpload = () => {
+        setUploading(true)
+        setError(null)
     }
     return (
-        <div>
-
+        <div className="space-y-2">
+            <IKUpload
+                fileName="test-upload.png"
+                onError={onError}
+                onSuccess={handleSuccess}
+                onUploadStart={handleStartUpload}
+                validateFile={(file: File) => {
+                    const validTypes = ["image/png", "image/jpeg", "image/webp"]
+                }}
+            />
         </div>
     )
 }
