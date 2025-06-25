2ea1edb4ae2e6ec58005bc1011c3fd984e89f697
Shivam Bisht <rajshivam2745@gmail.com>
2025-06-25 23:24:47 +0530

    razorpay order added

--- api/orders/route.ts
+++ api/orders/route.ts
@@ -1,4 +1,5 @@
 import { connectDB } from "@/db/db";
+import Order from "@/models/order.model";
 import { authOptions } from "@/utils/auth";
 import { getServerSession } from "next-auth";
 import { NextResponse } from "next/server";
@@ -20,7 +21,29 @@ export async function POST(request: Request) {
 
         const order = await razorpay.orders.create({
             amount: variant.price * 100,
+            currency: "USD",
+            receipt: `receipt-${Date.now()}`,
+            notes: {
+                productId: productId.toString()
+            }
+        })
+        const newOrder = await Order.create({
+            userId: session.user.id,
+            productId,
+            variant,
+            razorpayOrderId: order.id,
+            amount: variant.price * 100,
+            status: "pending"
+        })
+        return NextResponse.json({
+            orderId: order.id,
+            amount: order.amount,
+            currency: order.currency,
+            dbOrderID: newOrder._id
         })
     } catch (error) {
+        console.log(error);
+        return NextResponse.json({ error: "Something went wrong" }, { status: 500 })
+
     }
 }
